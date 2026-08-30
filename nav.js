da8826e9f846b0e4d47e5ed77fa4576c7c1454cf
PRMS-Systems <oadrianhg@gmail.com>
2026-08-30 12:56:43 -0600

    Update nav.js

--- nav.js
+++ nav.js
@@ -46,9 +46,14 @@
   nav.innerHTML = `
     <div class="nav-container">
 
-      <a class="nav-logo" href="/">
-        <img src="/logoch.png" alt="PRMS Architecture Lab">
-      </a>
+<a class="nav-logo" href="/">
+  <img src="/logoch.png" alt="PRMS Architecture Lab">
+
+  <span class="nav-logo-text">
+    <span class="nav-logo-name">PRMS Architecture Lab</span>
+    <span class="nav-logo-subtitle">Performance-Regulated Metabolic System</span>
+  </span>
+</a>
 
       <!-- MOBILE MENU BUTTON -->
       <button
